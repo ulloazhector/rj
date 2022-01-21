35efe12e17344e28f2536551b2e42beb351559e1
Héctor Ulloa <ulloazhector@gmail.com>
2022-01-21 03:13:49 -0300

    Desafío clase 12 (centrado)

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -4,7 +4,7 @@ import Item from "./Item";
 const ItemList = ({ items }) => {
 
     return (
-        <ul className="d-flex flex-wrap">
+        <ul className="d-flex flex-wrap justify-content-center">
                 {items?.map((b) => (
                     <Item key={b.id} item={b} />
                     ))}
